978b536d06011fc07522e63798d8daab41ab0ea3
Kunj Patel <90958919+Kunj-Pate1@users.noreply.github.com>
2022-10-09 16:42:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARJJXHHdbN_Lhy5oF7QP3aq9B5D9cg_WY",
-    authDomain: "cube-chatapp.firebaseapp.com",
-    projectId: "cube-chatapp",
-    storageBucket: "cube-chatapp.appspot.com",
-    messagingSenderId: "755485854264",
-    appId: "1:755485854264:web:3adad2d1cfde86c5c2b4cd",
-    measurementId: "G-ECEG47Z8LM"
+    //Enter your Firebase API-key here
  };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
